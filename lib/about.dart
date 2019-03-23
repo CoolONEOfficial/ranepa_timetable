5a989fde90e04847ed2adb181cdc020ec591478a
Vadim_0x01 <vadim11.12@mail.ru>
2019-03-23 22:55:52 +0300

    Fixed UI markup

--- lib/about.dart
+++ lib/about.dart
@@ -16,15 +16,11 @@ class AboutBackgroundClipper extends CustomClipper<Path> {
     final Path path = Path();
     path.lineTo(0.0, size.height);
 
-    var firstEndPoint = Offset(size.width * .5, size.height - 35.0);
-    var firstControlpoint = Offset(size.width * 0.25, size.height - 50.0);
-    path.quadraticBezierTo(firstControlpoint.dx, firstControlpoint.dy,
-        firstEndPoint.dx, firstEndPoint.dy);
+    var firstEndPoint = Offset(size.width, size.height);
+    var firstControlpoint = Offset(size.width * 0.50, size.height - 60.0);
+    path.quadraticBezierTo(firstControlpoint.dx, firstControlpoint.dy, firstEndPoint.dx, firstEndPoint.dy);
 
-    var secondEndPoint = Offset(size.width, size.height - 80.0);
-    var secondControlPoint = Offset(size.width * .75, size.height - 10);
-    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
-        secondEndPoint.dx, secondEndPoint.dy);
+
 
     path.lineTo(size.width, 0.0);
     path.close();
@@ -262,7 +258,7 @@ class HomeScreen extends StatelessWidget {
                   ClipPath(
                     clipper: AboutBackgroundClipper(),
                     child: Container(
-                      height: ScreenUtil().setHeight(1470),
+                      height: ScreenUtil().setHeight(1300),
                       decoration: BoxDecoration(
                         color: Colors.black
                       ),
