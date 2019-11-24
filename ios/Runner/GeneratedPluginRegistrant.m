33e5d07422f89dc44a07cc050b3d38e1b534879e
coolone <coolone.official@gmail.com>
2019-11-24 19:34:06 +0300

    Updated version to 1.7.2

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,42 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<android_intent/AndroidIntentPlugin.h>)
 #import <android_intent/AndroidIntentPlugin.h>
+#else
+@import android_intent;
+#endif
+
+#if __has_include(<device_calendar/DeviceCalendarPlugin.h>)
 #import <device_calendar/DeviceCalendarPlugin.h>
+#else
+@import device_calendar;
+#endif
+
+#if __has_include(<flutter_email_sender/FlutterEmailSenderPlugin.h>)
 #import <flutter_email_sender/FlutterEmailSenderPlugin.h>
+#else
+@import flutter_email_sender;
+#endif
+
+#if __has_include(<package_info/PackageInfoPlugin.h>)
 #import <package_info/PackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<shared_preferences/SharedPreferencesPlugin.h>)
 #import <shared_preferences/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<url_launcher/UrlLauncherPlugin.h>)
 #import <url_launcher/UrlLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
